5050c6b4d908aacbad8cb2a6ab7170c0aca82075
Jason Antwi-Appah <hey@jasonaa.me>
2020-10-20 14:05:39 -0500

    Added ability to get user likes and get streaming links

---
+++ main.py
@@ -0,0 +1,43 @@
+import soundcloud, b2sdk, os
+
+client = soundcloud.Client(
+    client_id=os.environ["sc_client_id"],
+    client_secret=os.environ["sc_client_secret"])
+
+def getUserLikes(userId):
+    getting = False
+    href = False
+    final = []
+    arr = []
+    if isinstance(userId, int):
+        userId = str(userId)
+    elif isinstance(userId, str):
+        tmpUsr = client.get('/resolve', url='https://soundcloud.com/'+userId)
+        userId = str(tmpUsr.id)
+    else:
+        raise AttributeError("The ID passed in is invalid. ID should be username as a string, or ID as an integer.")
+    likes = client.get('/users/' + userId + '/favorites', limit=100, linked_partitioning=1)
+    arr.extend(likes.collection)
+    while (getting == False or href == True):
+        getting = True
+        likes = client.get(likes.next_href, limit=100, linked_partitioning=1)
+        arr.extend(likes.collection)
+        try:
+            likes.next_href
+            href = True
+        except AttributeError:
+            href = False
+    for i in arr:
+        if (i.streamable):
+            final.append({"song": i.title, "id": i.id, "artist": i.user["username"], "artist_id": i.user["id"]})
+    return final
+
+def getDlLink(trackId):
+    track = client.get('/tracks/' + trackId)
+    stream_url = client.get(track.stream_url, allow_redirects=False)
+    return stream_url.location
+
+yeet = getUserLikes(88656944)
+#yeet = getUserLikes('jasonaa')
+for i in yeet:
+    print(i)
