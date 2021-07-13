d4ebed131f2a5014d50d636a79f63d9315a5e8c4
Jason Antwi-Appah <hey@jasonaa.me>
2021-07-12 22:40:07 -0500

    feat+refactor: create cli

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SCDownloader
+# sc-dl
+
+This is a simple CLI written in Python that downloads all of a SoundCloud user's liked tracks to a directory of a user's choice.
+
+## Running
+This project uses `poetry` for dependency management, so make sure you have that installed, then run `poetry install` to install the project's dependencies. Running the CLI is as simple as running `python3 main.py`. To view options that you can set, run `python3 main.py --help`
+
+PRs/issues welcome!
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import soundcloud
-import b2sdk
 import os
+import requests
+import click
 from dotenv import load_dotenv
 load_dotenv()
 
-client = soundcloud.Client(
-    client_id=os.environ["sc_client_id"],
-    client_secret=os.environ["sc_client_secret"])
-
-
-def getUserLikes(userId):
-    getting = False
-    href = False
-    final = []
-    arr = []
+def resolveUid(userId):
     if isinstance(userId, int):
         userId = str(userId)
     elif isinstance(userId, str):
@@ -22,32 +14,55 @@ def getUserLikes(userId):
     else:
         raise AttributeError(
             "The ID passed in is invalid. ID should be username as a string, or ID as an integer.")
-    likes = client.get('/users/' + userId + '/favorites',
-                       limit=100, linked_partitioning=1)
-    arr.extend(likes.collection)
-    while (getting == False or href == True):
-        getting = True
-        likes = client.get(likes.next_href, limit=100, linked_partitioning=1)
-        arr.extend(likes.collection)
-        try:
-            likes.next_href
-            href = True
-        except AttributeError:
-            href = False
-    for i in arr:
-        if (i.streamable):
-            final.append({"song": i.title, "id": i.id, "artist": i.user["username"], "artist_id": i.user[
-                         "id"], "streamable": i.streamable, "stream_url": i.stream_url if i.streamable else ""})
-    return final
+    return userId
 
+def getLikes(url):
+    print('Fetching a new page of likes...')
+    res = []
+    likes = client.get(url, linked_partitioning=1)
+    if (likes.reason != 'OK'):
+        raise Exception("An unexpected message was encountered when fetching liked tracks.", )
+    for track in likes.collection:
+        if (track.streamable):
+            res.append({"song": track.title, "id": track.id, "artist": track.user["username"], "artist_id": track.user[
+                         "id"], "streamable": track.streamable, "stream_url": track.stream_url if track.streamable else "",
+                         "download_url": track.download_url if track.streamable else ""})
+    if (likes.next_href):
+        return res+recursive(likes.next_href)
+    else:
+        print('Done fetching likes.')
+        return res
+        
+def dlSong(obj, path):
+    file = requests.get(obj["stream_url"], params={'client_id': os.environ['sc_client_id']})
+    f = open(path, "wb")
+    f.write(file.content)
+    f.close()
 
-def getDlLink(trackId):
-    track = client.get('/tracks/' + trackId)
-    stream_url = client.get(track.stream_url, allow_redirects=False)
-    return stream_url.location
+def dlSongs(songs, path):
+    print("Beginning download...")
+    if (os.path.exists(path) and os.path.isdir(path) == False):
+        raise Exception("The path passed in is either not a directory or nonexistent.")
+    for song in songs:
+        fname = f"{song['artist']} - {song['song']}.mp3".replace("/","_").replace("\\","_").replace(":","_")
+        fpath = os.path.join(path,fname)
+        print("Downloading...", fpath)
+        dlSong(song,fpath)
 
+@click.command()
+@click.option('--user', help='The username, or user id of the user whose likes you want to download.', required=True)
+@click.option('--path', help='The directory to download songs to.', required=True)
+@click.option('--client_id', default=os.environ["sc_client_id"], help='SoundCloud client ID used to authenticate with SoundCloud API. Defaults to the environment variable `sc_client_id` if not specified.')
+@click.option('--client_secret', default=os.environ["sc_client_secret"], help='SoundCloud client secret used to authenticate with SoundCloud API. Defaults to the environment variable `sc_client_secret` if not specified.')
+def go(user, path, client_id, client_secret):
+    """Download all of a SoundCloud user's liked tracks to a directory."""
+    print("Initializing SoundCloud client...")
+    global client
+    client = soundcloud.Client(
+        client_id=client_id,
+        client_secret=client_secret)
+    print("Client initialization successful. Getting a listing of tracks from SoundCloud.")
+    dlSongs(getLikes('/users/' + resolveUid(user) + '/likes/tracks'), path)
 
-yeet = getUserLikes(88656944)
-#yeet = getUserLikes('jasonaa')
-for i in yeet:
-    print(i)
+if __name__ == '__main__':
+    go()
